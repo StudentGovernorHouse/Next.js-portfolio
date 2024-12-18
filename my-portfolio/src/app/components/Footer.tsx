bf1f87c3a25ae3af1f234e29f768c70aacc9327d
Arfa Siddiqui <Arfasiddiqui2017@gmail.com>
2024-12-18 16:16:52 +0500

    Add files via upload

---
+++ my-portfolio/src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => (
+    <div className='mt-16 py-6 text-center bg-pink-500'>
+        |© AS | All Right Reserved 2024.|
+    </div>
+)
+
+export default Footer
